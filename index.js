ceba4d6cdb0ab78a01d011f86905cd7f442af2e0
onkarvyavahare2001 <onkarvyavahare2001@gmail.com>
2023-03-16 22:47:17 +0530

    Api Data Fetched

--- index.js
+++ index.js
@@ -1,13 +1,37 @@
 const fs = require("fs");
 const http = require("http");
+const requests = require("requests");
+
+const replaceWithData=(htmlContent,objInArray)=>{
+    let temperature=htmlContent.replace("{%tempVal%}",objInArray.main.temp);
+     temperature=temperature.replace("{%tempMin%}",objInArray.main.temp_min);
+     temperature=temperature.replace("{%tempMax%}",objInArray.main.temp_max);
+     temperature=temperature.replace("{%location%}",objInArray.name);
+     temperature=temperature.replace("{%country%}",objInArray.sys.country);
+    // console.log(temperature);
+    return temperature;
+}
 
 
+const readFile=fs.readFileSync("./home.html","utf-8");
 
-const readFile=fs.readFileSync("./home.html","utf-8",(err,))
+
 const server=http.createServer((req,res)=>{
-   
+    requests('https://api.openweathermap.org/data/2.5/forecast?id=524901&appid=f0fc12e0d0bc503ccfea5d7ab9d791f9')
+    .on('data',(chunk)=> {
+        const objData=JSON.parse(chunk);
+        const arrayOfObj=[objData];
+        
+        const realTimeData=arrayOfObj.map((val)=> replaceWithData(readFile,val)).join("");
+       res.write(realTimeData);
+    //   console.log(arrayOfObj);
+    })
+    .on('end', (err)=> {
+      if (err) return console.log('connection closed due to errors', err);
+     
+      res.end();
+    });
 });
-
 
 
 
