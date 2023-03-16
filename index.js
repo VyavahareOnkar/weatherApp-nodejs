aacf7c70358a3c93deaed83043c1ef404f7e41be
onkarvyavahare2001 <onkarvyavahare2001@gmail.com>
2023-03-17 01:14:27 +0530

    style.css in integrated with html

--- index.js
+++ index.js
@@ -1,32 +1,37 @@
 const fs = require("fs");
+
 const http = require("http");
 const requests = require("requests");
 
-const replaceWithData=(htmlContent,objInArray)=>{
-    let temperature=htmlContent.replace("{%tempVal%}",objInArray.main.temp);
+
+
+
+const readFile=fs.readFileSync("home.html","utf-8");
+
+
+const replaceWithData=(htmlfile,objInArray)=>{
+    let temperature=htmlfile.replace("{%tempVal%}",objInArray.main.temp);
      temperature=temperature.replace("{%tempMin%}",objInArray.main.temp_min);
      temperature=temperature.replace("{%tempMax%}",objInArray.main.temp_max);
      temperature=temperature.replace("{%location%}",objInArray.name);
      temperature=temperature.replace("{%country%}",objInArray.sys.country);
-    // console.log(temperature);
+     temperature=temperature.replace("{%tempstatus%}",objInArray.weather[0].main);
+    // console.log(objInArray.main);
     return temperature;
 }
 
-
-const readFile=fs.readFileSync("./home.html","utf-8");
-
-
 const server=http.createServer((req,res)=>{
-    requests('Enter api link here')
-    .on('data',(chunk)=> {
+    requests("https://api.openweathermap.org/data/2.5/weather?q=Pune,In&APPID=f0fc12e0d0bc503ccfea5d7ab9d791f9")
+    .on("data",(chunk)=> {
         const objData=JSON.parse(chunk);
         const arrayOfObj=[objData];
         
-        const realTimeData=arrayOfObj.map((val)=> replaceWithData(readFile,val)).join("");
+        let realTimeData=arrayOfObj.map((val)=> replaceWithData(readFile,val)).join("");
        res.write(realTimeData);
+       res.end();
     //   console.log(arrayOfObj);
     })
-    .on('end', (err)=> {
+    .on("end", (err)=> {
       if (err) return console.log('connection closed due to errors', err);
      
       res.end();
@@ -36,5 +41,7 @@ const server=http.createServer((req,res)=>{
 
 
 server.listen(8000,"127.0.0.1",()=>{
-    console.log("server has been created successfully!");
+    console.log("server started");
 });
+
+
